ef42c70f598cc0742dbe9c73b3fa648d9b0a2ca5
Смирнов Александр Вячеславович <aleksandrsmirnov@at-consulting.ru>
2015-12-02 18:59:24 +0300

    fix app

--- src/main/java/ru/sa2/examples/servlets/LolServlet.java
+++ src/main/java/ru/sa2/examples/servlets/LolServlet.java
@@ -1,10 +1,12 @@
 package ru.sa2.examples.servlets;
 
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 
 /**
  * Created by asmirnov on 02.12.15.
